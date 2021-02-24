077e99e0cfcdb307df38a7299237d0cd397bc2f5
Dhrubo <dhrubo.dh@gmail.com>
2021-02-24 14:59:17 -0500

    Added the header for the individual chatscreen

--- components/ChatListItem/index.tsx
+++ components/ChatListItem/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { View, Text, Image, TouchableWithoutFeedback } from "react-native";
 import { ChatRoom } from "../../types";
 import styles from "./styles";
+import { useNavigation } from "@react-navigation/native";
 
 export type ChatListItemProps = {
   chatRoom: ChatRoom;
@@ -11,27 +12,31 @@ export type ChatListItemProps = {
 const ChatListItem = (props: ChatListItemProps) => {
   const { chatRoom } = props;
 
+  const navigation = useNavigation()
+
   const user = chatRoom.users[1];
 
   const onclick = () => {
-    console.warn(`Clicked on ${user.name}`)
-  }
+    navigation.navigate('ChatRoom', { id: chatRoom.id, name: user.name })
+  };
 
   return (
     <TouchableWithoutFeedback onPress={onclick}>
-    <View style={styles.container}>
-      <View style={styles.leftContainer}>
-        <Image source={{ uri: user.imageUri }} style={styles.avatar} />
-        <View style={styles.midContainer}>
-          <Text style={styles.userName}>{user.name}</Text>
-          <Text numberOfLines={2} style={styles.lastMessage}>
-            {chatRoom.lastMessage.content}
-          </Text>
+      <View style={styles.container}>
+        <View style={styles.leftContainer}>
+          <Image source={{ uri: user.imageUri }} style={styles.avatar} />
+          <View style={styles.midContainer}>
+            <Text style={styles.userName}>{user.name}</Text>
+            <Text numberOfLines={2} style={styles.lastMessage}>
+              {chatRoom.lastMessage.content}
+            </Text>
+          </View>
         </View>
+        <Text style={styles.timeStamp}>
+          {" "}
+          {moment(chatRoom.lastMessage.createdAt).format("DD/MM/YYYY")}
+        </Text>
       </View>
-      <Text style={styles.timeStamp}> {moment(chatRoom.lastMessage.createdAt).format('DD/MM/YYYY')}
-      </Text>
-    </View>
     </TouchableWithoutFeedback>
   );
 };
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,23 +1,32 @@
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
-import { ColorSchemeName, View } from 'react-native';
-import { Octicons, MaterialCommunityIcons } from '@expo/vector-icons'
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
+import { ColorSchemeName, View } from "react-native";
+import { Octicons, MaterialCommunityIcons, MaterialIcons, FontAwesome5 } from "@expo/vector-icons";
 
-import NotFoundScreen from '../screens/NotFoundScreen';
-import { RootStackParamList } from '../types';
-import MainTabNavigator from './MainTabNavigator';
-import LinkingConfiguration from './LinkingConfiguration';
-import Colors from '../constants/Colors'
-import ChatRoomScreen from '../screens/ChatRoomScreen';
+import NotFoundScreen from "../screens/NotFoundScreen";
+import { RootStackParamList } from "../types";
+import MainTabNavigator from "./MainTabNavigator";
+import LinkingConfiguration from "./LinkingConfiguration";
+import Colors from "../constants/Colors";
+import ChatRoomScreen from "../screens/ChatRoomScreen";
 
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
-export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
+export default function Navigation({
+  colorScheme,
+}: {
+  colorScheme: ColorSchemeName;
+}) {
   return (
     <NavigationContainer
       linking={LinkingConfiguration}
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+    >
       <RootNavigator />
     </NavigationContainer>
   );
@@ -29,38 +38,68 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
   return (
-    <Stack.Navigator screenOptions={{ 
-      headerStyle: {
-        backgroundColor: Colors.light.tint,
-        shadowOpacity: 0,
-        elevation: 0
-      },
-      headerTintColor: Colors.light.background,
-      headerTitleAlign: 'left',
-      headerTitleStyle: {
-        fontWeight: 'bold'
-      }
-    }}>
-      <Stack.Screen 
-        name="Root" 
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: Colors.light.tint,
+          shadowOpacity: 0,
+          elevation: 0,
+        },
+        headerTintColor: Colors.light.background,
+        headerTitleAlign: "left",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen
+        name="Root"
         component={MainTabNavigator}
         options={{
-          title: 'Whisper',
+          title: "Whisper",
           headerRight: () => (
-            <View style={{
-              flexDirection: 'row',
+            <View
+              style={{
+                flexDirection: "row",
+                marginRight: 10,
+                width: 60,
+                justifyContent: "space-between",
+              }}
+            >
+              <Octicons name="search" size={22} color={"white"} />
+              <MaterialCommunityIcons
+                name="dots-vertical"
+                size={22}
+                color={"white"}
+              />
+            </View>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="ChatRoom"
+        component={ChatRoomScreen}
+        options={({ route }) => ({
+          title: route.params.name,
+          headerRight: () => (
+            <View               style={{
+              flexDirection: "row",
               marginRight: 10,
-              width: 60,
-              justifyContent: 'space-between'
+              width: 100,
+              justifyContent: "space-between",
             }}>
-              <Octicons name='search' size={22} color={'white'} />
+              <FontAwesome5 name='video' size={22} color={'white'} />
+              <MaterialIcons name='call' size={22} color={'white'} />
               <MaterialCommunityIcons name='dots-vertical' size={22} color={'white'} />
             </View>
           )
-        }}
+        })}
       />
-      <Stack.Screen name="ChatRoom" component={ChatRoomScreen} options={{ title: 'Chat Room' }} />
-      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
+      <Stack.Screen
+        name="NotFound"
+        component={NotFoundScreen}
+        options={{ title: "Oops!" }}
+      />
     </Stack.Navigator>
   );
 }
