045281422b75feef0b9f45c698c4b73a6f761aba
Dhrubo <dhrubo.dh@gmail.com>
2021-03-04 15:31:33 -0500

    install AWS amplify

--- App.tsx
+++ App.tsx
@@ -5,6 +5,10 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import Amplify from 'aws-amplify'
+// import awsmobile from './aws-exports.js'
+
+// Amplify.configure(awsmobile)
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
